bea0b0453c90702716ececcac176eb2591ef880b
jiin-jung <cki08543@gmail.com>
2025-08-25 06:21:53 +0900

    S3 Update4

--- src/main/java/com/zanchi/zanchi_backend/config/s3/PresignConfig.java
+++ src/main/java/com/zanchi/zanchi_backend/config/s3/PresignConfig.java
@@ -1,15 +1,26 @@
 package com.zanchi.zanchi_backend.config.s3;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class PresignConfig {
+
     @Bean
-    S3Presigner s3Presigner(@Value("${AWS_REGION:ap-northeast-2}") String region) {
-        return S3Presigner.builder().region(Region.of(region)).build();
+    public S3Client s3Client() {
+        // 자격증명은 기본 프로바이더 체인(EC2 role 등) 사용
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
     }
 }
